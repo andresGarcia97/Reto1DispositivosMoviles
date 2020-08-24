1e486bb6a3ba9c3628b3bf913f516d82fa34d7c9
andresGarcia97 <andresgarciatehortua@gmail.com>
2020-08-24 01:12:35 -0500

    Data picker funcionando
    -Metodo de comparación de fechas funcionando.
    -Errores de compilacion solucionados.

--- app/src/main/java/com/co/movil/reto1/Edad.java
+++ app/src/main/java/com/co/movil/reto1/Edad.java
@@ -2,14 +2,81 @@ package com.co.movil.reto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Edad extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Edad extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+    private TextView dateInScreen;
+    private Calendar currentlyDate;
+    private Calendar bornDate = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        currentlyDate = Calendar.getInstance();
         setContentView(R.layout.activity_edad);
+        dateInScreen = findViewById(R.id.fechaNacimiento);
+        findViewById(R.id.fechaNacimiento).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePicker();
+            }
+        });
     }
-    
+
+    private void showDatePicker () {
+        Calendar minimeYear = Calendar.getInstance();
+        Calendar maximeYear = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                currentlyDate.get(Calendar.YEAR), currentlyDate.get(Calendar.MONTH), currentlyDate.get(Calendar.DAY_OF_MONTH));
+        minimeYear.set(Calendar.YEAR, currentlyDate.get(Calendar.YEAR) - 50);
+        datePickerDialog.getDatePicker().setMinDate(minimeYear.getTimeInMillis());
+        maximeYear.set(Calendar.YEAR, currentlyDate.get(Calendar.YEAR) + 1);
+        datePickerDialog.getDatePicker().setMaxDate(maximeYear.getTimeInMillis());
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        bornDate = Calendar.getInstance();
+        bornDate.set(Calendar.YEAR,year);
+        bornDate.set(Calendar.MONTH ,month);
+        bornDate.set(Calendar.DAY_OF_MONTH,day);
+        String dateSelected = "Fecha:     "+year+" / "+(month + 1)+" / "+day;
+        dateInScreen.setText(dateSelected);
+    }
+
+    public void calcularEdad(View view) {
+        currentlyDate = Calendar.getInstance();
+        if(bornDate == null){
+            Toast.makeText(getApplicationContext(),R.string.noSeleccionoFecha,Toast.LENGTH_LONG).show();
+        }
+        else if(bornDate.get(Calendar.YEAR) == currentlyDate.get(Calendar.YEAR)  &&
+                bornDate.get(Calendar.MONTH) == currentlyDate.get(Calendar.MONTH) &&
+                bornDate.get(Calendar.DAY_OF_MONTH) == currentlyDate.get(Calendar.DAY_OF_MONTH)) {
+            Toast.makeText(getApplicationContext(),R.string.fechaIgual,Toast.LENGTH_LONG).show();
+        }
+
+        else if(bornDate.get(Calendar.YEAR) >= currentlyDate.get(Calendar.YEAR)  &&
+                (bornDate.get(Calendar.MONTH) >= currentlyDate.get(Calendar.MONTH) && bornDate.get(Calendar.DAY_OF_MONTH) >= currentlyDate.get(Calendar.DAY_OF_MONTH))) {
+            Toast.makeText(getApplicationContext(),R.string.fechaFutura,Toast.LENGTH_LONG).show();
+        }
+
+        else if(bornDate.get(Calendar.YEAR) <= currentlyDate.get(Calendar.YEAR)  &&
+                bornDate.get(Calendar.MONTH) <= currentlyDate.get(Calendar.MONTH) &&
+                bornDate.get(Calendar.DAY_OF_MONTH) <= currentlyDate.get(Calendar.DAY_OF_MONTH)) {
+            int years = currentlyDate.get(Calendar.YEAR) - bornDate.get(Calendar.YEAR) ;
+            int months = currentlyDate.get(Calendar.MONTH) - bornDate.get(Calendar.MONTH);
+            int days = currentlyDate.get(Calendar.DAY_OF_MONTH) - bornDate.get(Calendar.DAY_OF_MONTH) ;
+            String age = "Usted tiene: "+years+" AÑOS "+months+" MESES "+days+" DIAS ";
+            Toast.makeText(getApplicationContext(),age,Toast.LENGTH_LONG).show();
+        }
+    }
 }
--- app/src/main/java/com/co/movil/reto1/MainActivity.java
+++ app/src/main/java/com/co/movil/reto1/MainActivity.java
@@ -1,13 +1,14 @@
 package com.co.movil.reto1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void mostrarDerechos(View view){
-        Toast.makeText(getApplicationContext(),"SE fue",Toast.LENGTH_LONG);
+        Toast.makeText(getApplicationContext(),R.string.derechosAutor,Toast.LENGTH_LONG).show();
+    }
+
+    public void lanzarActivityCalcularEdad(View view) {
+        Intent intent = new Intent(this, Edad.class);
+        startActivity(intent);
     }
 }
 
